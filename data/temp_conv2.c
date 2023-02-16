5bcd0a2e12ac019839b218c06258dbd308fb09a4
LostWorld21 <112750776+LostWorld21@users.noreply.github.com>
2023-02-17 00:47:03 +0530

    Add files via upload

--- data/temp_conv2.c
+++ data/temp_conv2.c
@@ -1,10 +1,10 @@
-// Write a program to find centigrade for a given fahrenheit temperature
+// Write a program to find fahrenheit for a given centigrade temperature
 #include <stdio.h>
 int main(){
     float c, f;
     printf("Enter temperature in centigrade : ");
     scanf("%f", &c);
-    f = ((9*5)*c)+32;
-    printf("The temperature in fahrenheit is : %f", f);
+    f = ((9*c)/5)+32;
+    printf("The temperature in fahrenheit is : %f deg F", f);
     return 0;
 }
