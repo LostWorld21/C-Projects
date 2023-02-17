1b9e4d9998a4017276026829e4b8c8b7bd5c8ce8
LostWorld21 <112750776+LostWorld21@users.noreply.github.com>
2023-02-17 19:33:33 +0530

    Add files via upload

---
+++ data/digits.c
@@ -0,0 +1,14 @@
+// WAP to input 3 digit numbers and display the individual digits
+#include <stdio.h>
+int main(){
+    int num, d, s = 0;
+    printf("Enter a number : ");
+    scanf("%d", &num);
+    while(num!=0)
+    {
+        d = num % 10;
+        printf("%d", d);
+        num = num / 10;
+    }
+    return 0;
+}
