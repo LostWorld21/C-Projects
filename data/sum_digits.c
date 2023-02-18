d9fc6ef9816d1fc7a8fbb694f9976826d5f5c7e3
LostWorld21 <112750776+LostWorld21@users.noreply.github.com>
2023-02-19 00:04:43 +0530

    Add files via upload

--- data/sum_digits.c
+++ data/sum_digits.c
@@ -1,15 +1,16 @@
-// WAP to input 3 digit numbers and display the individual digits
+// WAP to input 4 digit number and find the sum of digits
 #include <stdio.h>
 int main()
 {
-    int num, d;
+    int num, d, s = 0;
     printf("Enter a number : ");
     scanf("%d", &num);
     while (num != 0)
     {
         d = num % 10;
-        printf("%d", d);
+        s = s + d;
         num = num / 10;
     }
+    printf("The sum of digits is : %d", s);
     return 0;
 }
