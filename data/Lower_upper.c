b226538ebb2cbab59a4847f642c33cf726efdfc6
LostWorld21 <112750776+LostWorld21@users.noreply.github.com>
2023-02-21 19:11:45 +0530

    Update Lower_upper.c

--- data/Lower_upper.c
+++ data/Lower_upper.c
@@ -2,12 +2,16 @@
 int main()
 {
 	char a;
-printf("Enter the alphabet: ");
-scanf("%c", &a);
-{
-	if (a >= 97 && a <= 122)
-	a= a-32;
-	printf("\nThe entered character is in lowercase \nAfter converting the character to uppercase is %c", a);
+	printf("Enter the alphabet: ");
+	scanf("%c", &a);
+	
+	if (a >= 97 && a <= 122) {
+		a= a-32;
+		printf("The entered character is in lowercase \nAfter converting the character to uppercase is %c", a);
 	}
-return 0;
+	else if(a >=65 && a <= 90)
+		printf("The entered character is already in Uppercase : %c",a);
+	else
+		printf("Invalid character");
+	return 0;
 }
