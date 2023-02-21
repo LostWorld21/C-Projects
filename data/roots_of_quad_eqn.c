c331d0e7f1d684ee466696b4c676927df2fc0e40
LostUniverse21 <125828656+lostuniverse21@users.noreply.github.com>
2023-02-21 19:32:02 +0530

    Update and rename Roots_of_quad_eqn.c to roots_of_quad_eqn.c

--- data/roots_of_quad_eqn.c
+++ data/roots_of_quad_eqn.c
@@ -1,24 +1,25 @@
+//WAP to determine the roots of a quadratic equation ax^2+bx+c=0 using if-else statement
 #include <stdio.h>
 #include <math.h>
 int main()
 {
-	float a, b, c, d, r1, r2;
+	float a, b, c, d, r1, r2; //d= determinant
     printf("Enter the value of a, b, c: ");
     scanf("%f %f %f", &a, &b, &c);
     d = (b * b) - (4 * a * c);
-    if (a==0 && b==0)
-    	printf("There is no solution of the quadratic equation");
+    if (a==0)
+    	printf("This isn't a quadratic equation");
 	else if (d==0)
     {
-    	r1=r2= -1*b/(2.0*a);
-    	printf("The value of roots of the quadratic equation is : %f", r1);
+    	r1=r2= -b/(2.0*a);
+    	printf("The quadratic equation have equal roots i.e., : %f", r1);
 	}
 	else if (d<0)
 		printf("The equation does not have real roots");
 	else
 	{
-    	r1 = (-b + (sqrt(b*b) - (4.0 * a * c)))/(2*a);
-    	r2 = (-b - (sqrt(b*b) - (4.0 * a * c)))/(2*a);
+    	r1 = (-b + sqrt(d))/(2*a);
+    	r2 = (-b - sqrt(d))/(2*a);
     	printf("The value of roots of the quadratic equation is : %f and %f", r1, r2);
 	}
     return 0;
