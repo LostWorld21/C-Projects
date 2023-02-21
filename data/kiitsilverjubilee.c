b5dbcad8a5f88b22e015b746bf1045001a0a5275
LostUniverse21 <125828656+lostuniverse21@users.noreply.github.com>
2023-02-22 02:13:42 +0530

    Create kiitsilverjubilee.c

---
+++ data/kiitsilverjubilee.c
@@ -0,0 +1,14 @@
+// Write a C program to display the message “KIIT Silver Jubilee” if the entered year is 25 otherwise display “Wrong input”.
+
+#include <stdio.h>
+int main ()
+{
+	int year;
+	printf("Enter the year: ");
+	scanf("%d", &year);
+	if(year == 2022)
+	printf("KIIT Silver Jubilee");
+	else
+	printf("Wrong Input");
+	return 0;
+}|
